848fbc9d0946d3174fcae9f29769074548004566
Hafeez <izzhafeez@gmail.com>
2022-08-13 23:48:42 +0800

    Readding assets

---
+++ src/assets/logo-controller.js
@@ -0,0 +1,120 @@
+import mom from './logo/mom.png';
+import scdf from './logo/scdf.png';
+import vrmc from './logo/vrmc.png';
+
+import angular from './logo/angular.png';
+import c from './logo/c.png';
+import chinese from './logo/chinese.png';
+import confucius from './logo/confucius.png';
+import cpp from './logo/cpp.png';
+import csharp from './logo/csharp.png';
+import css from './logo/css.png';
+import django from './logo/django.png';
+import english from './logo/english.png';
+import erstudio from './logo/er-studio.png';
+import excel from './logo/excel.png';
+import flask from './logo/flask.png';
+import french from './logo/french.png';
+import frontend from './logo/frontend.png';
+import hackerrank from './logo/hackerrank.png';
+import harvardx from './logo/harvardx.png';
+import html from './logo/html.png';
+import gcp from './logo/gcp.png';
+import ibm from './logo/ibm.png';
+import japanese from './logo/japanese.png';
+import java from './logo/java.png';
+import javascript from './logo/javascript.png';
+import jupyter from './logo/jupyter.png';
+import language from './logo/language.png';
+import latex from './logo/latex.png';
+import linkedin from './logo/linkedin.png';
+import malay from './logo/malay.png';
+import mongo from './logo/mongo.png';
+import musescore from './logo/musescore.png';
+import node from './logo/node.png';
+import nus from './logo/nus.png';
+import pandas from './logo/pandas.png';
+import powerbi from './logo/power-bi.png';
+import problemsolving from './logo/problem-solving.png';
+import python from './logo/python.png';
+import qgis from './logo/qgis.png';
+import r from './logo/r.png';
+import react from './logo/react.png';
+import ruby from './logo/ruby.png';
+import rust from './logo/rust.png';
+import sharepoint from './logo/sharepoint.png';
+import sms from './logo/sms.png';
+import spanish from './logo/spanish.png';
+import sql from './logo/sql.png';
+import ssms from './logo/ssms.png';
+import tableau from './logo/tableau.png';
+import telegram from './logo/telegram.png';
+import ti from './logo/ti.png';
+import tools from './logo/tools.png';
+import udemy from './logo/udemy.png';
+import uom from './logo/uom.png';
+import vex from './logo/vex.png';
+import vue from './logo/vue.png';
+
+const logos = {
+  mom: mom,
+  scdf: scdf,
+  vrmc: vrmc,
+
+  angular: angular,
+  c: c,
+  chinese: chinese,
+  confucius: confucius,
+  cpp: cpp,
+  csharp: csharp,
+  css: css,
+  django: django,
+  english: english,
+  erstudio: erstudio,
+  excel: excel,
+  flask: flask,
+  french: french,
+  frontend: frontend,
+  gcp: gcp,
+  hackerrank: hackerrank,
+  harvardx: harvardx,
+  html: html,
+  ibm: ibm,
+  japanese: japanese,
+  java: java,
+  javascript: javascript,
+  jupyter: jupyter,
+  language: language,
+  languages: language,
+  latex: latex,
+  linkedin: linkedin,
+  malay: malay,
+  mongo: mongo,
+  musescore: musescore,
+  node: node,
+  nus: nus,
+  pandas: pandas,
+  powerbi: powerbi,
+  problemsolving: problemsolving,
+  python: python,
+  qgis: qgis,
+  r: r,
+  react: react,
+  ruby: ruby,
+  rust: rust,
+  sharepoint: sharepoint,
+  sms: sms,
+  spanish: spanish,
+  sql: sql,
+  ssms: ssms,
+  tableau: tableau,
+  telegram: telegram,
+  ti: ti,
+  tools: tools,
+  udemy: udemy,
+  uom: uom,
+  vex: vex,
+  vue: vue
+}
+
+export default logos;
