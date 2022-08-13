86bb72dcffd8630a3e4e2523c4796769464eb09b
Hafeez <izzhafeez@gmail.com>
2022-08-13 23:48:14 +0800

    Edited Routing to enable react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import './index.css';
+import './components/Icon.css';
+import Navbar from './components/Navbar.js';
+
+import Home from './pages/Home';
+import Merits from './pages/Merits';
+import MeritsTemplate from './pages/MeritsTemplate';
+import ExperienceTemplate from './pages/ExperienceTemplate';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div className="content-container">
+        <div className="content-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="website-react" element={<Home />} />
+            <Route path="merits" element={<Merits />} />
+            <Route path="merits/experience/:name" element={<ExperienceTemplate />} />
+            <Route path="merits/certificates/:name" element={<MeritsTemplate dataPath="certificates"/>} />
+            <Route path="merits/languages/:name" element={<MeritsTemplate dataPath="languages"/>} />
+            <Route path="merits/tools/:name" element={<MeritsTemplate dataPath="tools"/>} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
