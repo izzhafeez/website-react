74389e84c6e0317821c9a9cf7c16e18da2d048c7
Hafeez <izzhafeez@gmail.com>
2022-08-14 23:21:34 +0800

    Edited Logo Controller - segregated the logos for improved readability

--- src/assets/logo-controller.js
+++ src/assets/logo-controller.js
@@ -1,120 +1,149 @@
 import mom from './logo/mom.png';
 import scdf from './logo/scdf.png';
 import vrmc from './logo/vrmc.png';
+import nus from './logo/nus.png';
 
-import angular from './logo/angular.png';
 import c from './logo/c.png';
 import chinese from './logo/chinese.png';
-import confucius from './logo/confucius.png';
 import cpp from './logo/cpp.png';
 import csharp from './logo/csharp.png';
 import css from './logo/css.png';
-import django from './logo/django.png';
 import english from './logo/english.png';
-import erstudio from './logo/er-studio.png';
-import excel from './logo/excel.png';
-import flask from './logo/flask.png';
 import french from './logo/french.png';
-import frontend from './logo/frontend.png';
-import hackerrank from './logo/hackerrank.png';
-import harvardx from './logo/harvardx.png';
 import html from './logo/html.png';
-import gcp from './logo/gcp.png';
-import ibm from './logo/ibm.png';
 import japanese from './logo/japanese.png';
 import java from './logo/java.png';
 import javascript from './logo/javascript.png';
-import jupyter from './logo/jupyter.png';
 import language from './logo/language.png';
-import latex from './logo/latex.png';
-import linkedin from './logo/linkedin.png';
 import malay from './logo/malay.png';
+import php from './logo/php.png';
+import python from './logo/python.png';
+import r from './logo/r.png';
+import ruby from './logo/ruby.png';
+import rust from './logo/rust.png';
+import spanish from './logo/spanish.png';
+import sql from './logo/sql.png';
+
+import confucius from './logo/confucius.png';
+import frontend from './logo/frontend.png';
+import hackerrank from './logo/hackerrank.png';
+import harvardx from './logo/harvardx.png';
+import ibm from './logo/ibm.png';
+import linkedin from './logo/linkedin.png';
+import sms from './logo/sms.png';
+import ti from './logo/ti.png';
+import udemy from './logo/udemy.png';
+import uom from './logo/uom.png';
+
+import angular from './logo/angular.png';
+import django from './logo/django.png';
+import erstudio from './logo/er-studio.png';
+import excel from './logo/excel.png';
+import flask from './logo/flask.png';
+import gcp from './logo/gcp.png';
+import jupyter from './logo/jupyter.png';
+import latex from './logo/latex.png';
 import mongo from './logo/mongo.png';
 import musescore from './logo/musescore.png';
 import node from './logo/node.png';
-import nus from './logo/nus.png';
 import pandas from './logo/pandas.png';
 import powerbi from './logo/power-bi.png';
-import problemsolving from './logo/problem-solving.png';
-import python from './logo/python.png';
 import qgis from './logo/qgis.png';
-import r from './logo/r.png';
 import react from './logo/react.png';
-import ruby from './logo/ruby.png';
-import rust from './logo/rust.png';
 import sharepoint from './logo/sharepoint.png';
-import sms from './logo/sms.png';
-import spanish from './logo/spanish.png';
-import sql from './logo/sql.png';
 import ssms from './logo/ssms.png';
 import tableau from './logo/tableau.png';
 import telegram from './logo/telegram.png';
-import ti from './logo/ti.png';
 import tools from './logo/tools.png';
-import udemy from './logo/udemy.png';
-import uom from './logo/uom.png';
 import vex from './logo/vex.png';
 import vue from './logo/vue.png';
 
-const logos = {
+import geospatial from './logo/geospatial.png';
+import mining from './logo/mining.png';
+import problemsolving from './logo/problem-solving.png';
+import web from './logo/web.png';
+
+const experiencesDict = {
   mom: mom,
   scdf: scdf,
   vrmc: vrmc,
+  nus: nus,
+}
 
-  angular: angular,
-  c: c,
-  chinese: chinese,
+const languagesDict = {
+  c: c, 
+  chinese: chinese, 
+  cpp: cpp, 
+  csharp: csharp, 
+  css: css, 
+  english: english, 
+  french: french, 
+  html: html, 
+  japanese: japanese, 
+  java: java, 
+  javascript: javascript, 
+  language: language, 
+  malay: malay, 
+  php: php, 
+  python: python, 
+  r: r, 
+  ruby: ruby, 
+  rust: rust, 
+  spanish: spanish, 
+  sql: sql, 
+}
+
+const certificatesDict = {
   confucius: confucius,
-  cpp: cpp,
-  csharp: csharp,
-  css: css,
+  frontend: frontend,
+  hackerrank: hackerrank,
+  harvardx: harvardx,
+  ibm: ibm,
+  linkedin: linkedin,
+  sms: sms,
+  ti: ti,
+  udemy: udemy,
+  uom: uom,
+}
+
+const toolsDict = {
+  angular: angular,
   django: django,
-  english: english,
   erstudio: erstudio,
   excel: excel,
   flask: flask,
-  french: french,
-  frontend: frontend,
   gcp: gcp,
-  hackerrank: hackerrank,
-  harvardx: harvardx,
-  html: html,
-  ibm: ibm,
-  japanese: japanese,
-  java: java,
-  javascript: javascript,
   jupyter: jupyter,
-  language: language,
-  languages: language,
   latex: latex,
-  linkedin: linkedin,
-  malay: malay,
   mongo: mongo,
   musescore: musescore,
   node: node,
-  nus: nus,
   pandas: pandas,
   powerbi: powerbi,
-  problemsolving: problemsolving,
-  python: python,
   qgis: qgis,
-  r: r,
   react: react,
-  ruby: ruby,
-  rust: rust,
   sharepoint: sharepoint,
-  sms: sms,
-  spanish: spanish,
-  sql: sql,
   ssms: ssms,
   tableau: tableau,
   telegram: telegram,
-  ti: ti,
   tools: tools,
-  udemy: udemy,
-  uom: uom,
   vex: vex,
-  vue: vue
+  vue: vue,
 }
+
+const skillsDict = {
+  geospatial: geospatial,
+  mining: mining,
+  problemsolving: problemsolving,
+  web: web,
+}
+
+var logos = {
+  ...experiencesDict,
+  ...languagesDict,
+  ...certificatesDict,
+  ...toolsDict,
+  ...skillsDict,
+};
 
 export default logos;
