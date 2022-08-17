01b0ce4823db813c2ab2474dd9444373ea06f710
Hafeez <izzhafeez@gmail.com>
2022-08-18 00:05:02 +0800

    Added more logos to Logo Controller

--- src/assets/logo-controller.js
+++ src/assets/logo-controller.js
@@ -15,6 +15,7 @@ import japanese from './logo/japanese.png';
 import java from './logo/java.png';
 import javascript from './logo/javascript.png';
 import language from './logo/language.png';
+import m from './logo/m.png';
 import malay from './logo/malay.png';
 import php from './logo/php.png';
 import python from './logo/python.png';
@@ -23,13 +24,19 @@ import ruby from './logo/ruby.png';
 import rust from './logo/rust.png';
 import spanish from './logo/spanish.png';
 import sql from './logo/sql.png';
+import vba from './logo/vba.png';
 
+import australianmc from './logo/australianmc.png';
+import awards from './logo/awards.png';
+import certificates from './logo/certificates.png';
 import confucius from './logo/confucius.png';
+import euclid from './logo/euclid.png';
 import frontend from './logo/frontend.png';
 import hackerrank from './logo/hackerrank.png';
 import harvardx from './logo/harvardx.png';
 import ibm from './logo/ibm.png';
 import linkedin from './logo/linkedin.png';
+import seamo from './logo/seamo.png';
 import sms from './logo/sms.png';
 import ti from './logo/ti.png';
 import udemy from './logo/udemy.png';
@@ -39,17 +46,21 @@ import angular from './logo/angular.png';
 import django from './logo/django.png';
 import erstudio from './logo/er-studio.png';
 import excel from './logo/excel.png';
+import express from './logo/express.png';
 import flask from './logo/flask.png';
 import gcp from './logo/gcp.png';
+import github from './logo/github.png';
 import jupyter from './logo/jupyter.png';
 import latex from './logo/latex.png';
 import mongo from './logo/mongo.png';
 import musescore from './logo/musescore.png';
 import node from './logo/node.png';
 import pandas from './logo/pandas.png';
+import postman from './logo/postman.png';
 import powerbi from './logo/power-bi.png';
 import qgis from './logo/qgis.png';
 import react from './logo/react.png';
+import selenium from './logo/selenium.png';
 import sharepoint from './logo/sharepoint.png';
 import ssms from './logo/ssms.png';
 import tableau from './logo/tableau.png';
@@ -58,10 +69,23 @@ import tools from './logo/tools.png';
 import vex from './logo/vex.png';
 import vue from './logo/vue.png';
 
+import analysis from './logo/analysis.png';
+import cleaning from './logo/cleaning.png';
+import databases from './logo/databases.png';
+import datascience from './logo/datascience.png';
+import extraction from './logo/extraction.png';
+import game from './logo/game.png';
 import geospatial from './logo/geospatial.png';
 import mining from './logo/mining.png';
+import minutes from './logo/minutes.png';
+import music from './logo/music.png';
 import problemsolving from './logo/problem-solving.png';
+import skills from './logo/skills.png';
+import visualization from './logo/visualization.png';
 import web from './logo/web.png';
+
+const publicDict = {
+}
 
 const experiencesDict = {
   mom: mom,
@@ -83,6 +107,8 @@ const languagesDict = {
   java: java, 
   javascript: javascript, 
   language: language, 
+  languages: language, 
+  m: m,
   malay: malay, 
   php: php, 
   python: python, 
@@ -91,15 +117,21 @@ const languagesDict = {
   rust: rust, 
   spanish: spanish, 
   sql: sql, 
+  vba: vba,
 }
 
 const certificatesDict = {
+  australianmc: australianmc,
+  awards: awards,
+  certificates: certificates,
   confucius: confucius,
+  euclid: euclid,
   frontend: frontend,
   hackerrank: hackerrank,
   harvardx: harvardx,
   ibm: ibm,
   linkedin: linkedin,
+  seamo: seamo,
   sms: sms,
   ti: ti,
   udemy: udemy,
@@ -111,17 +143,21 @@ const toolsDict = {
   django: django,
   erstudio: erstudio,
   excel: excel,
+  express: express,
   flask: flask,
   gcp: gcp,
+  github: github,
   jupyter: jupyter,
   latex: latex,
   mongo: mongo,
   musescore: musescore,
   node: node,
   pandas: pandas,
+  postman: postman,
   powerbi: powerbi,
   qgis: qgis,
   react: react,
+  selenium: selenium,
   sharepoint: sharepoint,
   ssms: ssms,
   tableau: tableau,
@@ -132,13 +168,24 @@ const toolsDict = {
 }
 
 const skillsDict = {
+  analysis: analysis,
+  cleaning: cleaning,
+  databases: databases,
+  datascience: datascience,
+  extraction: extraction,
+  game: game,
   geospatial: geospatial,
   mining: mining,
+  minutes: minutes,
+  music: music,
   problemsolving: problemsolving,
+  skills: skills,
+  visualization: visualization,
   web: web,
 }
 
 var logos = {
+  ...publicDict,
   ...experiencesDict,
   ...languagesDict,
   ...certificatesDict,
