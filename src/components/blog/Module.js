d87bb711708be924d6722fcd614e01d2341f8c05
Hafeez <izzhafeez@gmail.com>
2022-08-23 01:17:02 +0800

    Added blog folder for components

---
+++ src/components/blog/Module.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+
+import '../SmallPreview.css';
+
+import logos from '../../assets/logo-controller';
+
+import MicroIcon from '../icon/MicroIcon';
+
+function Module(props) {
+  return (
+    <div className="preview">
+      <div className="module-title">
+        <MicroIcon logo={logos["nus"]} alt="NUS"></MicroIcon>
+        {props.module.link &&
+          <Link to={`../blog/modules`}>
+            {props.module.title}
+          </Link>
+        }
+        {!props.module.link &&
+          <Link to={`../blog/modules/${props.module.key}`}>
+            {props.module.title}
+          </Link>
+        }
+        {props.module.starred && <star> ★</star>}
+      </div>
+      <div className="module-date">
+        {props.module.code}
+        &nbsp;
+        <span className={
+          props.module.grade.replace("+", "P").replace("-", "M")
+        }>
+          {
+            !props.module.link &&
+            "("+props.module.grade+")"
+          }
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Module;
