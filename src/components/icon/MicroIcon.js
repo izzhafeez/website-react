ae00539c12cab8c912eaf0928a010d9c8ef875ce
Hafeez <izzhafeez@gmail.com>
2022-08-23 01:17:22 +0800

    Added icon folder

---
+++ src/components/icon/MicroIcon.js
@@ -0,0 +1,9 @@
+function MicroIcon(props) {
+  return (
+    <div>
+      <img src={props.logo} alt={props.alt} className="micro-icon"></img>
+    </div>
+  );
+}
+
+export default MicroIcon;
---
+++ src/components/icon/MiniIcon.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function MiniIcon(props) {
+  return (
+    <div>
+      <Link to={`../merits/${props.kind}/${props.alt}`}>
+        <img src={props.logo} alt={props.alt} className="mini-icon"></img>
+        <div>{props.alt.split("-").join(" ").toUpperCase()}</div>
+      </Link>
+    </div>
+  );
+}
+
+export default MiniIcon;
