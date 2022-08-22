71d06b1780e2666560c5110bf06cbe9de6440d3a
Hafeez <izzhafeez@gmail.com>
2022-08-23 01:19:11 +0800

    Added blog page

---
+++ src/pages/blog/BlogLanding.js
@@ -0,0 +1,43 @@
+import { useParams } from 'react-router-dom';
+
+import './Blog.css';
+
+import modulesData from '../../data/modules.json';
+import notesData from '../../data/notes.json';
+import logos from '../../assets/logo-controller';
+
+import Module from '../../components/blog/Module';
+
+function BlogLanding(props) {
+  const dataDict = {
+    modules: modulesData,
+    notes: notesData
+  }
+
+  const params = useParams();
+  const kind = params.kind;
+  const data = dataDict[kind][kind];
+
+  const blogs = Object.keys(data).map((key, _) => {
+    const blog = data[key];
+    blog["key"] = key;
+
+    switch (kind) {
+      case ("modules"):
+        return blog.ignore ? null : <Module module={blog}/>;
+      default:
+        return "";
+    }
+    
+  });
+  return (
+    <div className="page">
+      <img src={logos["nus"]} alt={kind} className="icon"></img>
+      <br></br>
+      <h2>{kind.toUpperCase()}</h2>
+      <div className="blog-entries">{blogs}</div>
+    </div>
+  )
+}
+
+export default BlogLanding;
---
+++ src/pages/blog/ModulesDesc.js
@@ -0,0 +1,42 @@
+import { useParams } from 'react-router-dom';
+
+import './Blog.css';
+
+import modulesData from '../../data/modules.json';
+import notesData from '../../data/notes.json';
+import logos from '../../assets/logo-controller';
+
+import Note from '../../components/works/Note';
+import Text from '../../components/Text';
+
+function ModulesDesc(props) {
+  const { name } = useParams();
+  const data = modulesData.modules[name];
+  const description = [...data.description];
+  description.push(["DATE: " + data.date]);
+  description.push(["GRADE: " + data.grade]);
+
+  const notes = notesData.notes[name.toUpperCase()].contents.Topics.sublinks.map(link => {
+    const note = notesData.notes[link];
+    return <Note note={note}/>;
+  });
+
+  return (
+    <div className="page">
+      <img src={logos.nus} alt={data.title} className="icon"></img>
+      <br></br>
+      <Text
+        title={data.code.toUpperCase() + " " + data.title.toUpperCase()}
+        content={description}
+      ></Text>
+      <Text
+        title="TIPS"
+        content={data.tips}
+      ></Text>
+      <h2>TOPICS</h2>
+      <div className="notes-entries">{notes}</div>
+    </div>
+  );
+}
+
+export default ModulesDesc;
