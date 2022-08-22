a1c36c105de77302ce50008f4cb77fc084819c3c
Hafeez <izzhafeez@gmail.com>
2022-08-23 01:18:55 +0800

    Added notes folder for python and ruby scripts

---
+++ src/data/notes/notes.rb
@@ -0,0 +1,90 @@
+require 'csv'
+require 'json'
+
+notes = CSV.read("notes.csv", headers: true)
+notes_dict = {}
+
+def clean_markdown(text)
+  return text
+    .sub(/_s_/, "'s ")
+    .sub(/^\S{0,3}>/, "")
+    .gsub(/__/, " (")
+    .sub(/_$/, ")")
+    .gsub(/_/, " ")
+    .sub(/^\s/, "")
+end
+
+notes.reverse_each do |row|
+  course = row[1]
+  topic = row[2]
+  description = row[3]
+
+  if course == nil || description == nil
+    next
+  end
+
+  splitted_description = description.split("\n")
+  description_length = splitted_description.length
+
+  contents = {}
+  current_content = {}
+  splitted_description.each_with_index do |line, index|
+    if /[A-Z]/ =~ line[0] && !(/>/ =~ line)
+      current_content = {
+        "section" => line.sub(/<.*$/, ""),
+        "index" => index,
+        "points" => [],
+        "sublinks" => []
+      }
+      contents[line] = current_content
+    else
+      if !current_content.has_key?("points")
+        next
+      end
+      
+      if line[0] == "?"
+        current_content["sublinks"].append(
+          clean_markdown(line)
+        )
+      else
+        current_content["points"].append(
+        clean_markdown(line)
+      )
+      end
+    end
+  end
+
+  note_dict = {
+    "course" => course,
+    "parents" => [],
+    "children" => 0,
+    "topic" => topic,
+    "contents" => contents,
+    "date" => row[4],
+    "important" => row[5]
+  }
+  notes_dict[topic] = note_dict
+end
+
+notes_dict.each do |key, value|
+  value["contents"].each do |section, content|
+    sublinks = content["sublinks"]
+    sublinks.each do |sublink|
+      if notes_dict.has_key?(sublink)
+        notes_dict[sublink]["parents"].append(value["topic"])
+        value["children"] = value["children"] + 1
+      else
+        # puts sublink
+      end
+    end
+  end
+end
+
+sorted_dict = {}
+notes_dict.sort_by{|_key, value| -value["children"]}.each do |value|
+  sorted_dict[value[0]] = value[1]
+end
+
+File.open("../notes.json", "w") do |f|
+  f.write(JSON.pretty_generate({"notes" => sorted_dict}))
+end
