7cd66f34d154a5f9a16f28713e1a2250176a2094
Hafeez <izzhafeez@gmail.com>
2022-08-23 22:49:20 +0800

    Edited git-process.sh

--- git-process.sh
+++ git-process.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-for FILE in *
-do
-  for SUBFILE in $FILE
-  do
-    echo $SUBFILE
-    for SUBSUBFILE in $SUBFILE
+# for FILE in *
+# do
+#   for SUBFILE in $FILE
+#   do
+#     echo $SUBFILE
+#     for SUBSUBFILE in $SUBFILE
+#     do
+#       git add $SUBSUBFILE
+#       git commit -m "Edited $SUBSUBFILE"
+#     done
+#   done
+# done
+
+function loop_through_folder () {
+  if [[ $2 > 2 ]]
+  then
+    git add $1
+    git commit -m "Edited $1"
+  else
+    for FILES in "$1/*"
     do
-      git add $SUBSUBFILE
-      git commit -m "Edited $SUBSUBFILE"
+      for FILE in $FILES
+      do
+        if [[ $FILE == *"."* ]]
+        then
+          git add $FILE
+          git commit -m "Edited $FILE"
+        else
+          i=$(($2+1))
+          loop_through_folder $FILE $i
+        fi
+      done
     done
-  done
-done
+  fi
+}
 
-for FOLDER in src/*
+loop_through_folder src 1
+
+for FILES in "*"
 do
-  for FILE in $FOLDER
+  for FILE in $FILES
   do
-    echo $FILE
+    git add $FILE
+    git commit -m "Edited $FILE"
   done
 done
 
-# git push origin master
+git push origin master
