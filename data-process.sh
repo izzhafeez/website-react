102bf71a3eaf518cfb77ae18fa1866752eebbcbb
Hafeez <izzhafeez@gmail.com>
2022-08-23 21:32:18 +0800

    Edited data-process.sh

--- data-process.sh
+++ data-process.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Runs the Rust script that:
+# 1. Processes the logo folder
+# 2. Creates a js file that imports all those logos
 cd src/assets/logo-registration
 echo "Running Rust..."
 cargo run
 echo "Completed Rust."
 cd ../../..
 
+# Asks the user to run the Python script (that takes a long time)
+# Once yes, then retrieves from Google Sheets
+# And stores the data as a csv
 read -p "Run Sheets retrieval? (y/n) " to_run
 if [ $to_run == "y" ]
 then
@@ -15,12 +21,15 @@ then
   cd ../../..
 fi
 
+# Runs the Ruby script that processes the csv into json
+# So that the front end can use it
 cd src/data/notes
 echo "Running Ruby..."
 ruby notes.rb
 echo "Completed Ruby"
 cd ../../..
 
+# Runs the C script that counts how many lines I've written in each language
 cd src/data/coding-languages
 echo "Running C..."
 ./a.out
