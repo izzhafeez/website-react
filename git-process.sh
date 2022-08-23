9ad35524afd23127c0323eab821975029a8dde7a
Hafeez <izzhafeez@gmail.com>
2022-08-23 21:37:59 +0800

    Edited git-process.sh

--- git-process.sh
+++ git-process.sh
@@ -2,16 +2,10 @@
 
 for FILE in *
 do
-  git add $FILE
-  git commit -m "Edited $FILE"
   for SUBFILE in $FILE
   do
-    git add $SUBFILE
-    git commit -m "Edited $SUBFILE"
     for SUBSUBFILE in $SUBFILE
     do
-      git add $SUBSUBFILE
-      git commit -m "Edited $SUBSUBFILE"
       for SUB3FILE in $SUBSUBFILE
       do
         git add $SUB3FILE
