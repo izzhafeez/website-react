ff77d4dd4458aa63de9be29069b831833cd11dc9
Hafeez <izzhafeez@gmail.com>
2022-08-23 21:20:34 +0800

    Edited data-process.sh - minor changes

--- data-process.sh
+++ data-process.sh
@@ -9,14 +9,29 @@ cd ../../..
 read -p "Run Sheets retrieval? (y/n) " to_run
 if [ $to_run == "y" ]
 then
-  cd src/data
+  cd src/data/notes
   echo "Running Sheets retrieval..."
   python notes.py
-  cd ../..
+  cd ../../..
 fi
 
-cd src/data
+cd src/data/notes
 echo "Running Ruby..."
 ruby notes.rb
 echo "Completed Ruby"
+cd ../../..
 
+cd src/data/coding-languages
+echo "Running C..."
+./a.out
+echo "Completed C"
+cd ../../..
+
+for FILE in *
+do
+  for SUBFILE in $FILE
+  do
+    git add $SUBFILE
+    git commit -m "Edited $FILE - minor changes"
+  done
+done
