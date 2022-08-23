0df57f4aeee7cba56cb8e7faeec61cfc7c2383bd
Hafeez <izzhafeez@gmail.com>
2022-08-23 21:34:33 +0800

    Edited git-process.sh

--- git-process.sh
+++ git-process.sh
@@ -11,8 +11,14 @@ do
         git add $SUB3FILE
         git commit -m "Edited $SUB3FILE"
       done
+      git add $SUBSUBFILE
+      git commit -m "Edited $SUBSUBFILE"
     done
+    git add $SUBFILE
+    git commit -m "Edited $SUBFILE"
   done
+  git add $FILE
+  git commit -m "Edited $FILE"
 done
 
 git push origin master
