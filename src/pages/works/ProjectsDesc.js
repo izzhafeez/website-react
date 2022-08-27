74adfbcfe6a4078726ad7f51d5bcbdcbcaaad565
Hafeez <izzhafeez@gmail.com>
2022-08-27 20:46:42 +0800

    Edited src/pages/works

--- src/pages/works/ProjectsDesc.js
+++ src/pages/works/ProjectsDesc.js
@@ -11,58 +11,72 @@ import MiniIcon from '../../components/icon/MiniIcon';
 
 function ProjectsDesc() {
   const { name } = useParams();
-  const project = projectsData.projects[name];
-  const tools = project.tools.map((tool) => {
-    const toolData = toolsData.tools[tool];
-    return <MiniIcon logo={logos[toolData.img]} alt={tool} kind="tools"></MiniIcon>
-  });
-  const skills = project.skills.map((skill) => {
-    const skillData = skillsData.skills[skill];
-    return <MiniIcon logo={logos[skillData.img]} alt={skill} kind="skills"></MiniIcon>
-  });
-  const languages = project.languages.map((language) => {
-    const languageData = languagesData.languages[language];
-    return <MiniIcon logo={logos[languageData.img]} alt={language} kind="languages"></MiniIcon>
-  });
+  const projects = name === "all"
+    ? Object.values(projectsData.projects)
+    : [projectsData.projects[name]];
+  
+  const projectsContent = projects.filter(project => project.tools).map(project => {
+    console.log(project);
+    const tools = project.tools.map((tool) => {
+      const toolData = toolsData.tools[tool];
+      return <MiniIcon logo={logos[toolData.img]} alt={tool} kind="tools" key={tool}></MiniIcon>
+    });
+    const skills = project.skills.map((skill) => {
+      const skillData = skillsData.skills[skill];
+      return <MiniIcon logo={logos[skillData.img]} alt={skill} kind="skills" key={skill}></MiniIcon>
+    });
+    const languages = project.languages.map((language) => {
+      const languageData = languagesData.languages[language];
+      return <MiniIcon logo={logos[languageData.img]} alt={language} kind="languages" key={language}></MiniIcon>
+    });
 
-  const site = project.link
-    ? ["link", project.link, "LINK"]
-    : ["link", project.repo, "LINK", true];
+    const site = project.link
+      ? ["link", project.link, "LINK"]
+      : ["link", project.repo, "LINK", true];
+
+    return (
+      <div>
+        <img src={logos[project.img]} alt={project.title} className="icon"></img>
+        <br></br>
+        <Text
+          title={project.title.toUpperCase()}
+          content={project["full-description"]}
+        ></Text>
+
+        <Text
+          title="REPOSITORY"
+          content={
+            [
+              [
+                site
+              ]
+            ]
+          }
+        ></Text>
+
+        <Text
+          title="DURATION"
+          content={[[project.start+ " - " +project.end]]}
+        ></Text>
+        <h2>TOOLS</h2>
+        <div className="mini-icon-section">{tools}</div>
+        <br></br>
+        <h2>LANGUAGES</h2>
+        <div className="mini-icon-section">{languages}</div>
+        <br></br>
+        <h2>SKILLS</h2>
+        <div className="mini-icon-section">{skills}</div>
+        <br></br>
+        <br></br>
+      </div>
+    );
+  });
 
   return (
     <div className="page">
-      <img src={logos[project.img]} alt={project.title} className="icon"></img>
-      <br></br>
-      <Text
-        title={project.title.toUpperCase()}
-        content={project["full-description"]}
-      ></Text>
-
-      <Text
-        title="REPOSITORY"
-        content={
-          [
-            [
-              site
-            ]
-          ]
-        }
-      ></Text>
-
-      <Text
-        title="DURATION"
-        content={[[project.start+ " - " +project.end]]}
-      ></Text>
-      <h2>TOOLS</h2>
-      <div className="mini-icon-section">{tools}</div>
-      <br></br>
-      <h2>LANGUAGES</h2>
-      <div className="mini-icon-section">{languages}</div>
-      <br></br>
-      <h2>SKILLS</h2>
-      <div className="mini-icon-section">{skills}</div>
+      {projectsContent}
     </div>
-  );
+  )
 }
 
 export default ProjectsDesc;
