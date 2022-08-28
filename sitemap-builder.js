f46033afa7e680df2b23e7242af99a76fbe682ee
Hafeez <izzhafeez@gmail.com>
2022-08-28 22:46:11 +0800

    Edited sitemap-builder.js

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -12,7 +12,7 @@ const Sitemap = require('react-router-sitemap').default;
 function generateSitemap() {
 return (
     new Sitemap(router)
-        .build("http://izzhafeez.com")
+        .build("https://izzhafeez.com")
         .save("./public/sitemap.xml")
 );
 }
