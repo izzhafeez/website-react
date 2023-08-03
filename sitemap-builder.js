07d1c31a93d07dd00dbb028e69816bd94a15d768
Hafeez <izzhafeez@gmail.com>
2023-08-03 12:55:25 +0800

    Edited ./sitemap-builder.js

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -6,7 +6,7 @@ require("babel-register")({
 require.extensions['.css'] = () => {};
 require.extensions['.png'] = () => {};
 
-const router = require('./src2/router').default;
+const router = require('./src/routes/routes').default;
 const Sitemap = require('react-router-sitemap').default;
 
 function generateSitemap() {
