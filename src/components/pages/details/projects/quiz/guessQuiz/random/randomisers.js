6d5456a0d0944599982bc2de4300469dd76c518e
Hafeez <izzhafeez@gmail.com>
2023-08-18 14:37:00 +0800

    Fix randomisation of guess quiz

--- src/components/pages/details/projects/quiz/guessQuiz/random/randomisers.js
+++ src/components/pages/details/projects/quiz/guessQuiz/random/randomisers.js
@@ -1,10 +1,14 @@
+export const getRandom = () => {
+  return ((Date.now() % 1000) / 1000 + Math.random()) / 2;
+}
+
 export const shuffleRandomise = ({ arr, sort }) => {
   return arr.sort(sort);
 }; // just sort
 
 export const rotateRandomise = ({ arr, sort }) => {
   const sortedArr = arr.sort(sort);
-  const rotateAmount = Math.floor(Math.random() * arr.length);
+  const rotateAmount = Math.floor(getRandom() * arr.length);
   return [
     ...sortedArr.slice(rotateAmount, arr.length),
     ...sortedArr.slice(0, rotateAmount)
