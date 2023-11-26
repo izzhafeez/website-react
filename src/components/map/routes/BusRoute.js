92df5dfc58ce270f0a9ba05f9403d07f99c81c89
Izz Hafeez <izzhafeez@gmail.com>
2023-11-26 18:57:38 +0800

    Fix game

--- src/components/map/routes/BusRoute.js
+++ src/components/map/routes/BusRoute.js
@@ -1,5 +1,6 @@
 import Route from "./Route";
 import { Location } from "../locations";
+import busStops from "data/projects/json/quizzes/bus-stops.json";
 
 class BusRoute extends Route {
   constructor({ serviceNo, points }) {
@@ -10,11 +11,14 @@ class BusRoute extends Route {
   }
 
   getFeatures() {
-    return this.route.map(([lng, lat]) => new Location({
-      title: this.title,
-      latitude: lat,
-      longitude: lng
-    }).getFeature());
+    return this.route.map((id) => {
+      const stop = busStops[id]
+      return new Location({
+        title: stop.desc,
+        latitude: stop.lat,
+        longitude: stop.lng
+      }).getFeature();
+    })
   }
 };
 
